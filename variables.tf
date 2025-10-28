606d706501a0ad172a717903940d8ddc3e00a1d7
Sreekanth Manthena <sreekanth.clouddevops@gmail.com>
2025-10-28 16:57:27 +0530

    Fixed the variables file syntax issue

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-variable "aws_region" { type = string default = "us-east-1" }
-variable "ami"        { type = string default = "ami-0c02fb55956c7d316" } # change to valid AMI in region
-variable "instance_type" { type = string default = "t3.micro" }
-#variable "ssh_key_name" { type = string default = "" } # optional: AWS keypair name
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316" ##Amazon Linux 2 AMI (HVM), SSD Volume Type - us-east-1
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+  default     = "t3.micro"
+}
